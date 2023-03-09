4b7fcfe459fad4585d1a853bffbe34767c427479
Hector J Estrada Toledo <hjestrada@misena.edu.co>
2023-03-09 16:43:46 -0500

    first commit

---
+++ Comando Git.bat
@@ -0,0 +1,14 @@
+
+@ECHO OFF
+
+PowerShell.exe -Command "CD D:\PROYECTO JABONES"
+
+git init
+ 
+git add .
+ 
+git commit -m "first commit"
+ 
+git remote add origin https://github.com/hjestrada/Proyecto_Jabones.git
+ 
+git push -u origin master
