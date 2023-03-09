aef005252799dddb51c992d17b20ac2dafc5db92
Hector J Estrada Toledo <hjestrada@misena.edu.co>
2023-03-09 16:51:24 -0500

    Comando 4386

--- Comando Git.bat
+++ Comando Git.bat
@@ -4,9 +4,9 @@
 PowerShell.exe -Command "CD D:\PROYECTO JABONES"
 
 git add .
-git commit -m "Comando"
+git commit -m "Comando %RANDOM%"
  
 git remote add origin https://github.com/hjestrada/Proyecto_Jabones.git
  
 git push -u origin master
-pause
+pauser
